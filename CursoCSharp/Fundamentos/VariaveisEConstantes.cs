96978c7a2f8911e39b0220abef2f9740aaffc501
LEONARDO PEREIRA CW <a487czz@mmm.com>
2018-07-31 16:10:17 -0300

    start

---
+++ CursoCSharp/Fundamentos/VariaveisEConstantes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos
+{
+    class VariaveisEConstantes
+    {
+        public static void Executar() {
+            // Area da circunferencia
+            double raio = 4.5;
+            const double pi = 3.14;
+            
+            double area = pi * raio * raio;
+
+            Console.WriteLine("Area: " + area);
+
+            // Tipos internos
+            bool isNew = false;
+            Console.WriteLine("bool: " + isNew);
+
+            /*
+             * byte = 1 byte
+             * short = 2 bytes
+             * int = 4 bytes
+             * long = 8 bytes
+             */
+            byte idade = byte.MaxValue;
+            Console.WriteLine("byte: " + idade);
+
+            sbyte saldoDeGols = sbyte.MinValue;
+            Console.WriteLine("sbyte: " + saldoDeGols);
+
+            short salario = short.MaxValue;
+            Console.WriteLine("short: " + salario);
+
+            int number = int.MaxValue;
+            Console.WriteLine("int: " + number);
+            // uint = Positivo
+
+            long bigNumber = long.MaxValue;
+            Console.WriteLine("long: " + bigNumber);
+
+        }
+    }
+}
---
+++ CursoCSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            //PrimeiroPrograma.Executar();
+            //Comentarios.Executar();
+            VariaveisEConstantes.Executar();
+        }
+    }
+}
