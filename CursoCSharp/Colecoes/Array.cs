e44b7cddc47cbc40002dd4f21b463935af21e084
a487czz <leochp@gmail.com>
2018-08-08 09:19:48 -0300

    aula 62

--- CursoCSharp/Colecoes/Array.cs
+++ CursoCSharp/Colecoes/Array.cs
@@ -7,7 +7,7 @@ namespace CursoCSharp.Colecoes {
         public static void Executar() {
             // Arrays são estáticos
             string[] alunos = new string[5]; // Estrutura homogenea. Só podem ser armazenados dados do msm tipo
-            for (int i=0; i <= 4; i++) {
+            for (int i=0; i < alunos.Length; i++) {
                 alunos[i] = "a" + i.ToString();
             }
 
@@ -15,8 +15,22 @@ namespace CursoCSharp.Colecoes {
                 Console.WriteLine(x);
             }
 
+            double somatorio = 0;
             double media;
             double[] notas = { 9.7, 8.9, 8.5, 5.6 };
+
+            foreach( double nota in notas) {
+                somatorio += nota;
+            }
+
+            media = somatorio / notas.Length;
+
+            Console.WriteLine($"media {media}");
+
+            char[] palavra = { 'W', 'o', 'r', 'd' };
+            string word = new string(palavra);
+
+            Console.WriteLine(word);
             
         }
     }
