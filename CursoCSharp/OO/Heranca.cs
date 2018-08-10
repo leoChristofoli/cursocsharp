0114b95523da5f279e297a61bba16f0fde84eb23
a487czz <leochp@gmail.com>
2018-08-10 10:10:26 -0300

    aula 75

--- CursoCSharp/OO/Heranca.cs
+++ CursoCSharp/OO/Heranca.cs
@@ -2,12 +2,63 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace CursoCSharp.OO
-{
-    class Heranca
-    {
-        public static void Executar() {
+namespace CursoCSharp.OO {
 
+    public class Carro {
+        protected readonly int VelocidadeMaxima;
+        int VelocidadeAtual; //Private
+
+        public Carro(int VelocidadeMaxima) {
+            this.VelocidadeMaxima = VelocidadeMaxima;
+        }
+
+        protected int AlterarVelocidade(int delta) {
+            int novaVelocidade = VelocidadeAtual + delta;
+
+            if (novaVelocidade < 0) {
+                VelocidadeAtual = 0;
+            } else if (novaVelocidade > VelocidadeMaxima) {
+                VelocidadeAtual = VelocidadeMaxima;
+            } else {
+                VelocidadeAtual = novaVelocidade;
+            }
+
+            return VelocidadeAtual;
+        }
+
+        public int Acelerar() {
+            return AlterarVelocidade(5);
+        }
+
+        public int Frear() {
+            return AlterarVelocidade(-5);
+        }
+
+    }
+
+    public class Uno : Carro {
+        // Explicitamente constroe a class pai
+        public Uno(int velocidadeMaxima) : base(velocidadeMaxima) {
+
+        }
+    }
+
+    public class Ferrari: Carro {
+        public Ferrari(): base(350) {
+
+        }
+    }
+
+    class Heranca {
+        public static void Executar() {
+            Console.WriteLine("Uno");
+
+            Uno carro1 = new Uno(160);
+            Console.WriteLine(carro1.Acelerar());
+            Console.WriteLine(carro1.Acelerar());
+            Console.WriteLine(carro1.Frear());
+
+            Carro ferrari = new Ferrari();
         }
     }
 }
